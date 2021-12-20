431f6991ac9b4d12294eaf3ad90e270bfabd199b
Kevin Chevalier <kevin.chevalier10@gmail.com>
2021-12-19 17:59:41 -1000

    created difficulty and bomb placement
    close #3

--- minesweeper/source/main.py
+++ minesweeper/source/main.py
@@ -1,5 +1,7 @@
 import pygame as pg
 from pygame.locals import *
+from difficulty import Difficulty 
+from random import randrange
 
 purple = (85,11,202)
 
@@ -16,10 +18,40 @@ def main():
     setup()
     clock = pg.time.Clock()
     
-    tile_size = 64
     
-    rows = 2
-    columns = 16
+
+    mode = Difficulty.hard
+
+    if(mode==Difficulty.easy):
+        tile_size = 64
+        rows =  9
+        columns = 9
+        bombs = 10
+    elif(mode==Difficulty.medium):
+        tile_size = 42
+        rows =  16
+        columns = 16
+        bombs = 40
+    elif(mode==Difficulty.hard):
+        tile_size = 42
+        rows =16
+        columns = 30
+        bombs = 99
+    else:
+        print("ERROR!!! Invalid difficulty mode")
+        pg.quit()
+        return
+
+    bomb_present = [[False for y in range(rows)] for x in range(columns)]
+
+    bombs_placed = 0
+
+    while bombs_placed < bombs:
+        bomb_row = randrange(0,rows)
+        bomb_col = randrange(0,columns)
+        if not bomb_present[bomb_row][bomb_col]: 
+            bomb_present[bomb_row][bomb_col] = True
+            bombs_placed += 1
     
     width, height = columns*tile_size, rows*tile_size
     g = pg.display.set_mode((width, height))
