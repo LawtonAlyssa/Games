658cdbed9c7647806d827abfc0a7995314f09880
Alyssa <alawton1@my.hpu.edu>
2022-01-13 22:46:35 -1000

    Create game over screen, center text, reset logic
    close #5

--- minesweeper/source/main.py
+++ minesweeper/source/main.py
@@ -1,7 +1,9 @@
+import imp
 import pygame as pg
 from pygame.locals import *
 from difficulty import Difficulty 
 from random import randrange
+from typing import List, Tuple
 
 purple = (85,11,202)
 
@@ -44,24 +46,55 @@ def fill_bombs_present(rows, columns, bombs):
 
     return bombs_present
 
-def render_digits(my_font):
-    return [my_font.render(str(digit), False, (255, 255, 255)) for digit in range(1,9)]
+def render_digits(digits_font):
+    return [digits_font.render(str(digit), False, (255, 255, 255)) for digit in range(1,9)]
 
-def draw_digits(g, rows, columns, tile_size, bombs_present, digits):
+def draw_digits(g, rows, columns, tile_size, bombs_present, digits, digits_font):
     for y in range(rows):
         for x in range(columns):
             if bombs_present[y][x] is not True:
                 if bombs_present[y][x] != 0:
-                    g.blit(digits[bombs_present[y][x]-1],(x*tile_size,y*tile_size))
+                    digit = bombs_present[y][x]
+                    g.blit(digits[digit-1],center_text(width=tile_size, height=tile_size, text=str(digit), font=digits_font, x_offset=x*tile_size,y_offset=y*tile_size))
+                    
+def render_game_over(game_over_font:pg.font.Font, bombs_found:int, **kwargs) -> List[Tuple[pg.Surface,Tuple[int,int]]] : 
+    """renders game over text
 
+    Args:
+        game_over_font (pg.font.Font): font for game over
+        bombs_found (int): number of bombs found
+        **kwargs: window_width (int), window_height (int)
+
+    Returns:
+        List[Tuple[pg.Surface,Tuple[int,int]]]: blit arguments for each line
+    """    
+    out = []
+    for text, y_offset in [("Game Over", -50), ("Bombs Found: "+str(bombs_found), 50)]:
+        rendered = game_over_font.render(text, False, (255, 255, 255))
+        offset = center_text(text=text, font=game_over_font, y_offset=y_offset, **kwargs)
+        out.append((rendered, offset))
+    return out
+    
+def draw_game_over(g, game_over_rendered):
+    for text_tuple in game_over_rendered:
+        g.blit(*text_tuple)
+
+def center_text(width, height, text, font, x_offset=0, y_offset=0):
+    text_width, text_height = font.size(text) #txt being whatever str you're rendering
+    return (width - text_width)//2 + x_offset, (height - text_height)//2 + y_offset
+  
+def reset_game(rows, columns, bombs):
+    bombs_present = fill_bombs_present(rows, columns, bombs) # reset game
+    return bombs_present
+      
 def main():
     setup()
     clock = pg.time.Clock()
     
-    my_font = pg.font.SysFont('Comic Sans MS', 30)
-    digits = render_digits(my_font)
+    digits_font = pg.font.SysFont('Comic Sans MS', 30)
+    digits = render_digits(digits_font)
     
-    mode = Difficulty.easy
+    mode = Difficulty.hard
 
     if(mode==Difficulty.easy):
         tile_size = 64
@@ -84,12 +117,17 @@ def main():
         return
 
     bombs_present = fill_bombs_present(rows, columns, bombs)
+    bombs_found = 0
+    
 
-    width, height = columns*tile_size, rows*tile_size
-    g = pg.display.set_mode((width, height))
+    window_width, window_height = columns*tile_size, rows*tile_size
+    g = pg.display.set_mode((window_width, window_height))
 
+    game_over_font = pg.font.SysFont('Comic Sans MS', 60)
+    game_over_rendered = render_game_over(game_over_font, bombs_found, width=window_width, height=window_height)
+    
     while True:
-        g.fill((0, 0, 0))
+        g.fill((0,0,0))
         
         for event in pg.event.get():
             if event.type == QUIT:
@@ -97,11 +135,12 @@ def main():
                 return
     
         # draw here
-        
         draw_grid(**locals()) 
-        draw_digits(g, rows, columns, tile_size, bombs_present, digits)
+        draw_digits(g, rows, columns, tile_size, bombs_present, digits, digits_font)
         
         # add game logic here
+        # bombs_present = reset_game(rows, columns, bombs)
+        # draw_game_over(g, game_over_rendered)
         
         pg.display.flip()
         clock.tick(60)
