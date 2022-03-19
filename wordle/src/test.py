66d1133bd4bb63e254ca4d766323338a807b659a
Kevin Chevalier <kevin.chevalier10@gmail.com>
2022-03-18 21:02:35 -1000

    wordle - clean up test logic

--- wordle/src/test.py
+++ wordle/src/test.py
@@ -10,23 +10,29 @@ def runTest(name, func, sol, *args, **kwargs):
     except:
         crashed = True
         attempt = None
+        print("\n------------------------------------------------------")
         traceback.print_exc()
     elapsedTime = time() - startTime
     passTest = attempt == sol
-    print("{}: {} ({}s)".format(name, "CRASH" if crashed else ("PASS" if passTest else "Fail"), round(elapsedTime, 3)))
-    print("\n------------------------------------------------------")
+    print("{:16}: {} ({}s){}".format(name, "CRASH" if crashed else ("PASS" if passTest else "FAIL"), round(elapsedTime, 3), "" if passTest else " - Attempt: {}".format(attempt)))
     return 1 if passTest else 0
 
 def positiveWordFiltrationTest():
-    testCount = 2
-
-    correct = 0
-    correct += runTest("pos1", positiveListFiltration, ["abc"], letter_locs=[0,1,2], word_solution="abc", words_list=["abc", "def"])
-    correct += runTest("pos2", positiveListFiltration, ["abc", "dec"], letter_locs=[2], word_solution="abc", words_list=["abc", "def", "dec"])
-    print("Total:", round(100 * correct / testCount, 2), "%")
+    return [
+        runTest("pos1", positiveListFiltration, ["abc"], letter_locs=[0,1,2], word_solution="abc", words_list=["abc", "def"]),
+        runTest("pos2", positiveListFiltration, ["abc", "dec"], letter_locs=[2], word_solution="abc", words_list=["abc", "def", "dec"]),
+        runTest("pos3", positiveListFiltration, [], letter_locs=[0,1,2], word_solution="abc", words_list=["aaa", "bbb", "ccc", "abb", "aac", "aba"]),
+        runTest("pos4", positiveListFiltration, ["add"], letter_locs=[0], word_solution="abc", words_list=["add", "bbc", "bcc", "cbc"]),
+        runTest("pos5", positiveListFiltration, ["abc", "def", "dec"], letter_locs=[], word_solution="abc", words_list=["abc", "def", "dec"]),
+    ]
+    
 
 def main():
-    positiveWordFiltrationTest()
+    print("Running Tests\n")
+    runs = []
+    runs += positiveWordFiltrationTest()
+    print("--------------------------------------------")
+    print("Total: {}/{} ({}%)".format(sum(runs), len(runs), round(100 * sum(runs) / len(runs), 2)))
 
 if __name__=="__main__":
     main()
